4d11bba18a747f0bd13c80d8a8f5e11a15225f62
Lucas Mazza <luc4smazza@gmail.com>
2011-11-06 17:22:52 -0200

    add some specs for the initializers store

--- spec/page_spec.js
+++ spec/page_spec.js
@@ -4,4 +4,26 @@ describe('Page', function() {
     expect(page).toBeDefined();
   })
 
+  var block, another;
+
+  beforeEach(function() {
+    block = function() { }
+    another = function() { }
+    page.clear();
+  })
+
+  it('stores a initializer block', function() {
+    page('a-scope', block)
+
+    var initializers = page.initializers['a-scope'];
+    expect(initializers).toContain(block);
+  })
+
+  it('stores multiple blocks', function() {
+    page('a-scope', block)
+    page('a-scope', another)
+
+    var initializers = page.initializers['a-scope'];
+    expect(initializers.length).toBe(2);
+  })
 })
